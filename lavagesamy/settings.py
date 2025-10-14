d126691a9ee99b51c5eed6f3b0eb4fdf29032306
leloeduk <leloeduk2025@gmail.com>
2025-10-14 21:58:57 +0100

    feat: use local database

--- lavagesamy/settings.py
+++ lavagesamy/settings.py
@@ -96,11 +96,10 @@ WSGI_APPLICATION = 'lavagesamy.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),  
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  
+    }
 }
 
 
